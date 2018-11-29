90e37c9282dd4561261c5ec6228a83f2bdbc87b4
slowlyfly <34782890+slowlyfly@users.noreply.github.com>
2018-11-29 14:35:57 +0000

    Update dodgeCat_1.0.ino

--- dodgeCat_1.0.ino
+++ dodgeCat_1.0.ino
@@ -82,10 +82,10 @@ void loop() {
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
     delay(6);                       // waits 6ms for the servo to reach the position
   }
-    delay(50);                       // waits 15ms for the servo to reach the position
+    delay(50);                       // waits 50ms  
   for (pos = 40; pos >= 0; pos -= 1) { // goes from 40 degrees to 0 degrees    draw back the cat hand
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(10); 
+    delay(10);                       // waits 10ms for the servo to reach the position
   }
  }
 }
