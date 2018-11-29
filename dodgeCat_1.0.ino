39143504baae2258b95fc8bc51d625535f7c6f4a
slowlyfly <34782890+slowlyfly@users.noreply.github.com>
2018-11-29 10:08:53 +0000

    Update dodgeCat_1.0.ino

--- dodgeCat_1.0.ino
+++ dodgeCat_1.0.ino
@@ -1,37 +1,5 @@
 /*******************************************************************************
-
- Bare Conductive Proximity LED Fader
- -----------------------------------
- 
- Prox_LED.ino - maps the intensity of the onboard D13 LED on the Touch Board to 
- the proximity of a hand to an 85mm square electrode connected to E0 on the board
- 
- Based on code by Jim Lindblom and plenty of inspiration from the Freescale 
- Semiconductor datasheets and application notes.
- 
- Bare Conductive code written by Stefan Dzisiewski-Smith.
- 
- This work is licensed under a MIT license https://opensource.org/licenses/MIT
- 
- Copyright (c) 2016, Bare Conductive
- 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
- 
- The above copyright notice and this permission notice shall be included in all
- copies or substantial portions of the Software.
- 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- SOFTWARE.
+This is GID Gizmo project by Group 22
 
 *******************************************************************************/
 
